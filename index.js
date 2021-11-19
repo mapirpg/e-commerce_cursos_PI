3ed2599739a67a64cd68d77ec55c20db95f21fc1
ThiagoNunesPucci <56443751+ThiagoNunesPucci@users.noreply.github.com>
2021-11-19 20:22:43 -0300

    home

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+
 app.get('/', (req,res)=>{
     res.render('paginaInicial',{
         pageTitle: 'Venda de Cursos',
