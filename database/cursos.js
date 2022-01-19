3b0d45b6c182961c27997adbe898b649edcca26e
Marcel Perin <mapirpg@gmail.com>
2022-01-19 20:44:47 -0300

    update: paginaCuros

---
+++ database/cursos.js
@@ -0,0 +1,36 @@
+const cursos = [
+  {
+    nome: "Harmonização facial",
+    img: '../public/images/cursos/cur_harm__facial.png',
+    cargaHoraria: 50,
+    professor: 'Maria Fernanda da Graça',
+    valor: 0.00,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dicta eius tempore totam quis beatae nulla, exercitationem incidunt cupiditate autem consequatur hic dolores, amet eum reiciendis aut consectetur, veniam repellendus.' 
+  },
+  {
+    nome: "Depilação",
+    img: '../public/images/cursos/depilacao.png',
+    cargaHoraria: 40,
+    professor: 'José da Silva Prado',
+    valor: 0,
+    descricao: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, inventore voluptate, commodi, ad maiores velit aut nam laudantium ab hic delectus. Consequatur architecto quod suscipit cum deleniti, voluptas enim molestias.'
+  },
+  {
+    nome: "Sobrancelha",
+    img: '../public/images/cursos/cur_sobrancelha.png',
+    cargaHoraria: 100,
+    professor: 'Adalberta Reginalda',
+    valor: 100.00,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio molestiae laborum odit velit eius recusandae excepturi quidem ipsa autem tenetur, id, ad ea mollitia officia iusto saepe repellendus eaque soluta!' 
+  },
+  {
+    nome: "Maquiagem",
+    img: '/images/cursos/cur_maquiagem.png',
+    cargaHoraria: 200,
+    professor: 'Aderbaldo Garcia',
+    valor: 500.00,
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, natus? Veritatis, quo inventore vitae deleniti illum doloribus quasi officiis, dicta, quia perspiciatis laborum ullam. Necessitatibus eaque corrupti deserunt molestias? Delectus!'
+  }
+];
+
+module.exports = cursos
--- router/cursosRoute.js
+++ router/cursosRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const cursos = require('../database/cursos')
 
 const router = express.Router()
 
 router.get('/', (req, res,) => {
-    res.render('paginaCursos',);
+    res.render('paginaCursos', {cursos});
   });
   
-  module.exports = router;
-
+module.exports = router;
