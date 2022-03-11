ca932bc317e1bb462cee984b11e86d579a99a4e4
Maurício Costa <mauriciocosta23@gmail.com>
2022-03-10 21:01:55 -0300

    update: css/ejs - adminPainel

--- router/adminRoute.js
+++ router/adminRoute.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res,) => {
-    res.render('paginaAdmin');
+  res.render('admin');
+});
+
+router.get('/cadastrar_aluno', (req, res,) => {
+    res.render('adminCadastrarAluno');
   });
   
+  // router.get('/cadastrar_professor', (req, res,) => {
+  //   res.render('adminCadastrarProfessor');
+  // });
+
   module.exports = router;
